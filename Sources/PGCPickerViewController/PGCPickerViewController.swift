4d73dd9496983b66eaebb1e52f907280eafd5003
Paul Aguilar Catari <aguilarpgc@gmail.com>
2018-10-13 12:35:00 -0500

    renamed controller

---
+++ Sources/PGCPickerViewController/PGCPickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PGCPickerViewController.swift
+//  
+//
+//  Created by Paul Aguilar on 10/13/18.
+//
+
+import UIKit
+
+class PGCPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
